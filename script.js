d918a91dd0380fb6d1b49889a866f8d4d91b7460
Vladimir <nvovap@gmail.com>
2020-10-01 20:11:44 +0300

    second

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-$("#channel-add-ussd").click(function(event) {
-    event.preventDefault();
+
+
+$(document).ready(function () {
+    $("#msgid").html("This is Hello World by JQuery");
+
+    $("#channel-add-ussd").on('click', function (event) {
+        event.preventDefault();
+        $(".ussd").append('<li><span>*167#</span><em><button class="btn-ussd-delete">X</button></em></li>')
+    });
+
+
+    $("#channel-show-ussd").on('click', function (event) {
+        event.preventDefault();
+        $(".ussd li").each((index, li) => {
+            if (index != 0) console.log(li.innerText);
+        })
+    });
+
+
+    /*
+    $(".btn-ussd-delete").on('click', function (event) {
+        event.preventDefault();
+        $(this).parents("li").remove();
+    });
+    */
+
+    $(document).on('click', ".btn-ussd-delete", function () {
+        event.preventDefault();
+        $(this).parents("li").remove();
+    });
+
+
+
+
 });
